9bed2eaa0e87069cf4c21fd10223b903d4f58679
Mateus da Silva Sousa <mateus.sousa@teknisa.com>
2023-07-28 13:35:26 -0300

    change things

--- lua/passocacornio/packer.lua
+++ lua/passocacornio/packer.lua
@@ -1,133 +1,157 @@
 local is_bootstrap = false
 
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
-  vim.cmd [[packadd packer.nvim]]
+	is_bootstrap = true
+	vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+	vim.cmd([[packadd packer.nvim]])
 end
 
-require('packer').startup(function(use)
-  -- Package manager
-  use 'wbthomason/packer.nvim'
+require("packer").startup(function(use)
+	-- Package manager
+	use("wbthomason/packer.nvim")
 
-  use { -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    requires = {
-      -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
+	use({
+		"j-hui/fidget.nvim",
+		tag = "legacy",
+	})
 
-      -- Useful status updates for LSP
-      'j-hui/fidget.nvim',
+	use({
+		-- LSP Configuration & Plugins
+		"neovim/nvim-lspconfig",
+		requires = {
+			-- Automatically install LSPs to stdpath for neovim
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
 
-      -- Additional lua configuration, makes nvim stuff amazing
-      'folke/neodev.nvim',
-    },
-  }
+			-- Useful status updates for LSP
+			"j-hui/fidget.nvim",
 
-  use { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-  }
-  use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
+			-- Additional lua configuration, makes nvim stuff amazing
+			"folke/neodev.nvim",
+		},
+	})
 
-  use { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
-    end,
-  }
+	use({
+		-- Autocompletion
+		"hrsh7th/nvim-cmp",
+		requires = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
+	})
+	use({ "tzachar/cmp-tabnine", run = "./install.sh", requires = "hrsh7th/nvim-cmp" })
 
-  use { -- Additional text objects via treesitter
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    after = 'nvim-treesitter',
-  }
+	use({
+		-- Highlight, edit, and navigate code
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
+		end,
+	})
 
-  -- Git related plugins
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-rhubarb'
-  use 'lewis6991/gitsigns.nvim'
+	use({
+		-- Additional text objects via treesitter
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		after = "nvim-treesitter",
+	})
 
-  use 'navarasu/onedark.nvim' -- Theme inspired by Atom
-  use {
-	  'dracula/vim',
-	  config = function()
-		  as = 'dracula'
-		  -- vim.cmd('colorscheme dracula')
-	  end
-  }
-  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
-  use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-  use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+	-- Git related plugins
+	use("tpope/vim-fugitive")
+	use("tpope/vim-rhubarb")
+	use("lewis6991/gitsigns.nvim")
 
-  -- Fuzzy Finder (files, lsp, etc)
-  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+	use("navarasu/onedark.nvim") -- Theme inspired by Atom
+	use({
+		"dracula/vim",
+		config = function()
+			as = "dracula"
+			-- vim.cmd('colorscheme dracula')
+		end,
+	})
+	use("nvim-lualine/lualine.nvim") -- Fancier statusline
+	use("lukas-reineke/indent-blankline.nvim") -- Add indentation guides even on blank lines
+	use("numToStr/Comment.nvim") -- "gc" to comment visual regions/lines
+	use("tpope/vim-sleuth") -- Detect tabstop and shiftwidth automatically
 
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
-  use 'mbbill/undotree'
-  use 'onsails/lspkind.nvim'
-  use 'segeljakt/vim-silicon'
+	-- Fuzzy Finder (files, lsp, etc)
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x", requires = { "nvim-lua/plenary.nvim" } })
 
-  use {
-    "jose-elias-alvarez/null-ls.nvim",
-    "jay-babu/mason-null-ls.nvim",
-  }
---  use { 'jiangmiao/auto-pairs' }
-  use { 'windwp/nvim-ts-autotag' }
-  use 'cohama/lexima.vim'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-commentary'
-  use { 'folke/which-key.nvim',
-    config = function ()
-      require("which-key").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-    end
-  }
+	-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make", cond = vim.fn.executable("make") == 1 })
+	use("mbbill/undotree")
+	use("onsails/lspkind.nvim")
+	use("segeljakt/vim-silicon")
 
-  use {'akinsho/bufferline.nvim',
-    config = function ()
-      require('bufferline').setup({
-        options = {
-          mode = 'buffers',
-          offsets = {
-            {filetype = 'netrw'}
-          },
-        },
-        highlights = {
-          buffer_selected = {
-            italic = false
-          },
-          indicator_selected = {
-            fg = {attribute = 'fg', highlight = 'Function'},
-            italic = false
-          }
-        }
-      })
-    end
-  }
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		"jay-babu/mason-null-ls.nvim",
+	})
+	--  use { 'jiangmiao/auto-pairs' }
+	use({ "windwp/nvim-ts-autotag" })
+	use("cohama/lexima.vim")
+	use("tpope/vim-surround")
+	use("tpope/vim-commentary")
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			require("which-key").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
 
-  use 'kyazdani42/nvim-web-devicons'
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use 'mattn/emmet-vim'
-  use 'kristijanhusak/vim-carbon-now-sh'
-  -- use "lukas-reineke/lsp-format.nvim"
-  -- use "chrisbra/Colorizer"
+	use({
+		"akinsho/bufferline.nvim",
+		config = function()
+			require("bufferline").setup({
+				options = {
+					mode = "buffers",
+					offsets = {
+						{ filetype = "netrw" },
+					},
+				},
+				highlights = {
+					buffer_selected = {
+						italic = false,
+					},
+					indicator_selected = {
+						fg = { attribute = "fg", highlight = "Function" },
+						italic = false,
+					},
+				},
+			})
+		end,
+	})
 
-  -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
-  local has_plugins, plugins = pcall(require, 'custom.plugins')
-  if has_plugins then
-    plugins(use)
-  end
+	use("kyazdani42/nvim-web-devicons")
+	use({ "catppuccin/nvim", as = "catppuccin" })
+	use("mattn/emmet-vim")
+	use("kristijanhusak/vim-carbon-now-sh")
+	use({
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({
+				"css",
+				"sass",
+				"scss",
+				"vue",
+				"javascript",
+				"typescript",
+			}, { mode = "foreground" })
+		end,
+	})
+	-- use "lukas-reineke/lsp-format.nvim"
+	-- use "chrisbra/Colorizer"
 
-  if is_bootstrap then
-    require('packer').sync()
-  end
+	-- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
+	local has_plugins, plugins = pcall(require, "custom.plugins")
+	if has_plugins then
+		plugins(use)
+	end
+
+	if is_bootstrap then
+		require("packer").sync()
+	end
 end)
 
 -- When we are bootstrapping a configuration, it doesn't
@@ -135,12 +159,10 @@ end)
 --
 -- You'll need to restart nvim, and then it will work.
 if is_bootstrap then
-  print '=================================='
-  print '    Plugins are being installed'
-  print '    Wait until Packer completes,'
-  print '       then restart nvim'
-  print '=================================='
-  return
+	print("==================================")
+	print("    Plugins are being installed")
+	print("    Wait until Packer completes,")
+	print("       then restart nvim")
+	print("==================================")
+	return
 end
-
-
